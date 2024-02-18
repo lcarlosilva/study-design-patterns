37e0eae5ef3dd730ef185e1dadf16a91bac59095
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-18 19:29:56 -0300

    create class main to run program

--- src/main/kotlin/abstract_factory_method/Main_AbstractFactoryMethod.kt
+++ src/main/kotlin/abstract_factory_method/Main_AbstractFactoryMethod.kt
@@ -1,5 +1,25 @@
 package abstract_factory_method
 
-fun main(args: Array<String>) {
+import abstract_factory_method.app.Application
+import abstract_factory_method.factories.ITransportFactory
+import abstract_factory_method.factories.NineNineTransport
+import abstract_factory_method.factories.UberTransport
 
+fun main(args: Array<String>) {
+    val application: Application = Main_AbstractFactoryMethod.configureApplication("99")
+    application.startRoute()
+}
+
+class Main_AbstractFactoryMethod {
+    companion object {
+        fun configureApplication(company: String): Application {
+            return Application(
+                if (company == "99") {
+                    UberTransport()
+                } else {
+                    NineNineTransport()
+                }
+            )
+        }
+    }
 }
