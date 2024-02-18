5a7178c176839f9925252a9d99981cdc9765cda2
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-18 19:29:16 -0300

    create application to start routes transports

--- src/main/kotlin/abstract_factory_method/app/Application.kt
+++ src/main/kotlin/abstract_factory_method/app/Application.kt
@@ -1,4 +1,18 @@
 package abstract_factory_method.app
 
-class Application {
+import abstract_factory_method.aircrafts.IAircraft
+import abstract_factory_method.factories.ITransportFactory
+import abstract_factory_method.land_vehicle.ILandVehicle
+import abstract_factory_method.marine.IBoats
+
+class Application(factory: ITransportFactory) {
+    private val vehicle: ILandVehicle = factory.createTransportVehicle()
+    private val aircraft: IAircraft = factory.createTransportAircraft()
+    private val marine: IBoats = factory.createTransportMarine()
+
+    fun startRoute() {
+        vehicle.startRoute()
+        aircraft.startRoute()
+        marine.startRoute()
+    }
 }
