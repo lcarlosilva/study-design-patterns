6473cc0e9667e81eb481061928e1fd447416b7b1
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-18 19:27:46 -0300

    create impl types companies transport

--- src/main/kotlin/abstract_factory_method/factories/NineNineTransport.kt
+++ src/main/kotlin/abstract_factory_method/factories/NineNineTransport.kt
@@ -1,4 +1,22 @@
 package abstract_factory_method.factories
 
-class NineNineTransport {
+import abstract_factory_method.aircrafts.Helicopter
+import abstract_factory_method.aircrafts.IAircraft
+import abstract_factory_method.land_vehicle.ILandVehicle
+import abstract_factory_method.land_vehicle.Motorcycle
+import abstract_factory_method.marine.CruiseShip
+import abstract_factory_method.marine.IBoats
+
+class NineNineTransport : ITransportFactory {
+    override fun createTransportVehicle(): ILandVehicle {
+        return Motorcycle()
+    }
+
+    override fun createTransportAircraft(): IAircraft {
+        return Helicopter()
+    }
+
+    override fun createTransportMarine(): IBoats {
+        return CruiseShip()
+    }
 }
--- src/main/kotlin/abstract_factory_method/factories/UberTransport.kt
+++ src/main/kotlin/abstract_factory_method/factories/UberTransport.kt
@@ -1,4 +1,22 @@
 package abstract_factory_method.factories
 
-class UberTransport {
+import abstract_factory_method.aircrafts.Airplane
+import abstract_factory_method.aircrafts.IAircraft
+import abstract_factory_method.land_vehicle.Car
+import abstract_factory_method.land_vehicle.ILandVehicle
+import abstract_factory_method.marine.IBoats
+import abstract_factory_method.marine.Ship
+
+class UberTransport : ITransportFactory {
+    override fun createTransportVehicle(): ILandVehicle {
+        return Car()
+    }
+
+    override fun createTransportAircraft(): IAircraft {
+        return Airplane()
+    }
+
+    override fun createTransportMarine(): IBoats {
+        return Ship()
+    }
 }
