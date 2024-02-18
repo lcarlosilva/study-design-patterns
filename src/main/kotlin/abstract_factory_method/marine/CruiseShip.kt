e05b52bd3f465529538024d0f44cddb84ada78f5
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-18 19:24:27 -0300

    create types marine

--- src/main/kotlin/abstract_factory_method/marine/CruiseShip.kt
+++ src/main/kotlin/abstract_factory_method/marine/CruiseShip.kt
@@ -1,4 +1,17 @@
 package abstract_factory_method.marine
 
-class CruiseShip {
+class CruiseShip : IBoats {
+    override fun startRoute() {
+        getCargo()
+        anchorCollected()
+        println("Iniciando navegação a bombordo.")
+    }
+
+    override fun getCargo() {
+        println("Passageiros embarcados.")
+    }
+
+    override fun anchorCollected() {
+        println("Âncora recolhida")
+    }
 }
--- src/main/kotlin/abstract_factory_method/marine/Ship.kt
+++ src/main/kotlin/abstract_factory_method/marine/Ship.kt
@@ -1,4 +1,17 @@
 package abstract_factory_method.marine
 
-class Ship {
+class Ship : IBoats{
+    override fun startRoute() {
+        getCargo()
+        anchorCollected()
+        println("Iniciando navegação a bombordo.")
+    }
+
+    override fun getCargo() {
+        println("Containers todos embarcados!")
+    }
+
+    override fun anchorCollected() {
+        println("Âncora recolhida.")
+    }
 }
