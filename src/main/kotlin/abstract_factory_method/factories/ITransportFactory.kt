b921d2cb1eed17450c06abcb8c27fa711a8a2e9f
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-18 19:26:37 -0300

    create interfaces Factory companies

--- src/main/kotlin/abstract_factory_method/factories/ITransportFactory.kt
+++ src/main/kotlin/abstract_factory_method/factories/ITransportFactory.kt
@@ -2,8 +2,10 @@ package abstract_factory_method.factories
 
 import abstract_factory_method.aircrafts.IAircraft
 import abstract_factory_method.land_vehicle.ILandVehicle
+import abstract_factory_method.marine.IBoats
 
-interface ITransport {
+interface ITransportFactory {
     fun createTransportVehicle() : ILandVehicle
     fun createTransportAircraft() : IAircraft
+    fun createTransportMarine() : IBoats
 }
