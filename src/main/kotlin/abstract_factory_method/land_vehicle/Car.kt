49afcf6bc331d0574578a6a9085ccb10401551e5
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-18 19:21:28 -0300

    create land vehicle

--- src/main/kotlin/abstract_factory_method/land_vehicle/Car.kt
+++ src/main/kotlin/abstract_factory_method/land_vehicle/Car.kt
@@ -1,4 +1,12 @@
 package abstract_factory_method.land_vehicle
 
-class Car {
+class Car : ILandVehicle {
+    override fun startRoute() {
+        getCargo()
+        println("Iniciando o trajeto.")
+    }
+
+    override fun getCargo() {
+        println("Pegamos os passageiros, estamos prontos.")
+    }
 }
--- src/main/kotlin/abstract_factory_method/land_vehicle/Motorcycle.kt
+++ src/main/kotlin/abstract_factory_method/land_vehicle/Motorcycle.kt
@@ -1,4 +1,12 @@
 package abstract_factory_method.land_vehicle
 
-class Motorcycle {
+class Motorcycle : ILandVehicle {
+    override fun startRoute() {
+        getCargo()
+        println("Estamos iniciando a entrega.")
+    }
+
+    override fun getCargo() {
+        println("Já pegamos a encomenda!")
+    }
 }
