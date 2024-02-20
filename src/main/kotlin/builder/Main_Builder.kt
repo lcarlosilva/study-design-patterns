7e8a34cde2e4a5723165620794b05b96d7377ef3
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-20 19:50:56 -0300

    create file main example builder

--- src/main/kotlin/builder/Main_Builder.kt
+++ src/main/kotlin/builder/Main_Builder.kt
@@ -1,2 +1,26 @@
 package builder;
 
+import builder.builders.CarBuilder
+import builder.builders.TruckBuilder
+import builder.director.Director
+
+fun main() {
+
+    val director = Director()
+
+    val carBuilder = CarBuilder()
+    director.constructSedanCar(carBuilder)
+    println(carBuilder.result())
+
+    val truckBuilder = TruckBuilder()
+    director.constructTrucker(truckBuilder)
+    println(truckBuilder.result())
+
+    val sportCarBuilder = CarBuilder()
+    director.constructSportCar(sportCarBuilder)
+    println(sportCarBuilder.result())
+
+    val carManualBuilder = CarBuilder()
+    director.constructCar(carManualBuilder)
+    println(carManualBuilder.result())
+}
