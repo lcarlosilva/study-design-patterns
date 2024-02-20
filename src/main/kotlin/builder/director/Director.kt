711c309d8de16d16a7a11079aa3bf093fe384d00
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-20 19:50:22 -0300

    create director of builder

--- src/main/kotlin/builder/director/Director.kt
+++ src/main/kotlin/builder/director/Director.kt
@@ -1,4 +1,36 @@
 package builder.director
 
+import builder.builders.IVehicleBuilder
+import builder.components.Engine
+import builder.components.Transmission
+import builder.components.VehicleType
+
 class Director {
+    fun constructSedanCar(builder: IVehicleBuilder) {
+        builder.setVehicleType(VehicleType.SEDAN)
+        builder.setSeats(5)
+        builder.setTransmission(Transmission.AUTOMATIC)
+        builder.setEngine(Engine(1600))
+    }
+
+    fun constructTrucker(builder: IVehicleBuilder) {
+        builder.setVehicleType(VehicleType.TRUCK)
+        builder.setSeats(2)
+        builder.setTransmission(Transmission.AUTOMATIC_SEQUENTIAL)
+        builder.setEngine(Engine(13000))
+    }
+
+    fun constructSportCar(builder: IVehicleBuilder) {
+        builder.setVehicleType(VehicleType.SPORT_CAR)
+        builder.setSeats(2)
+        builder.setTransmission(Transmission.AUTOMATIC_SEQUENTIAL)
+        builder.setEngine(Engine(4600))
+    }
+
+    fun constructCar(builder: IVehicleBuilder) {
+        builder.setVehicleType(VehicleType.SEDAN)
+        builder.setSeats(4)
+        builder.setTransmission(Transmission.MANUAL)
+        builder.setEngine(Engine(4600))
+    }
 }
