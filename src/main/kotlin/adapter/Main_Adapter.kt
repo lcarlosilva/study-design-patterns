2d9613ed5424de0d3bd8c1c9631f1b494fad15cf
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-20 22:04:25 -0300

    create class main for run code

--- src/main/kotlin/adapter/Main_Adapter.kt
+++ src/main/kotlin/adapter/Main_Adapter.kt
@@ -1,2 +1,19 @@
 package adapter
 
+import adapter.adapters.MercadoPagoAdapter
+import adapter.adapters.PayoneerAdapter
+import adapter.mercado_pago.MercadoPago
+import adapter.payoneer.Payoneer
+import adapter.paypal.IPaypalPayments
+
+fun main() {
+    val paypalWithPayoneer: IPaypalPayments
+    paypalWithPayoneer = PayoneerAdapter(Payoneer())
+    paypalWithPayoneer.paypalPayment()
+    paypalWithPayoneer.paypalReceive()
+
+    val paypalWithMercadoPAgo: IPaypalPayments
+    paypalWithMercadoPAgo = MercadoPagoAdapter(MercadoPago())
+    paypalWithMercadoPAgo.paypalPayment()
+    paypalWithMercadoPAgo.paypalReceive()
+}
