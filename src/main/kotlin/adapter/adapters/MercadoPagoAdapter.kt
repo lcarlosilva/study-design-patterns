57f4bea800cd24dec977c1ac7c4ddb3698002e6a
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-20 22:03:09 -0300

    create Adapters

--- src/main/kotlin/adapter/adapters/MercadoPagoAdapter.kt
+++ src/main/kotlin/adapter/adapters/MercadoPagoAdapter.kt
@@ -1,4 +1,22 @@
 package adapter.adapters
 
-class MercadoPagoAdapter {
+import adapter.mercado_pago.MercadoPago
+import adapter.paypal.IPaypalPayments
+import adapter.utils.Token
+
+class MercadoPagoAdapter(
+    private val mercadoPago: MercadoPago
+): IPaypalPayments {
+    private lateinit var token: Token
+
+    override fun authToken() = Token()
+
+    override fun paypalPayment() {
+        this.token = authToken()
+        mercadoPago.sendPayment()
+    }
+
+    override fun paypalReceive() {
+        mercadoPago.receivePayment()
+    }
 }
--- src/main/kotlin/adapter/adapters/PayoneerAdapter.kt
+++ src/main/kotlin/adapter/adapters/PayoneerAdapter.kt
@@ -1,4 +1,22 @@
 package adapter.adapters
 
-class PayoneerAdapter {
+import adapter.payoneer.Payoneer
+import adapter.paypal.IPaypalPayments
+import adapter.utils.Token
+
+class PayoneerAdapter(
+    private val payoneer: Payoneer
+) : IPaypalPayments {
+    private lateinit var token: Token
+
+    override fun authToken() = Token()
+
+    override fun paypalPayment() {
+        this.token = authToken()
+        payoneer.sendPayment()
+    }
+
+    override fun paypalReceive() {
+        payoneer.receivePayment()
+    }
 }
