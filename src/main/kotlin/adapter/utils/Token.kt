5834aa57bcbf4dad8a75bcbe8ab811b316e5d621
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-02-20 22:03:43 -0300

    create token class

--- src/main/kotlin/adapter/utils/Token.kt
+++ src/main/kotlin/adapter/utils/Token.kt
@@ -1,4 +1,6 @@
 package adapter.utils
 
 class Token {
+    val token: String =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkRlc2lnbiBQYXR0ZXJuIiwiaWF0IjoxNTE2MjM5MDIyfQ.sHcQsrRkt_4PsIxi8H16a4Xix5sSV1rj9FXzOBQNaBU"
 }
